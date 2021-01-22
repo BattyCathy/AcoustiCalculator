c942adeedb874378a22dee3b609aa518f738600e
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-22 13:04:25 -0800

    Initial Commit

---
+++ AcoustiCalculator/AcoustiCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcoustiCalculatorApp.swift
+//  AcoustiCalculator
+//
+//  Created by Guy on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct AcoustiCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
